95b682d7b206322ede25a15c382dac2585927db2
AnitaOwen <anitaowen@pursuit.org>
2024-04-11 13:19:57 -0400

    added function to format the date in Item.jsx

--- src/Components/Items/Item.jsx
+++ src/Components/Items/Item.jsx
@@ -1,10 +1,15 @@
 
-const Item = ({ item }) => {
+const Item = ({ item }) => { 
+
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        return date.toLocaleDateString();
+    }
   return (
     <>
     <div>
         <h5>{item.name} <span>[{item.category}]</span></h5>
-        <p><span>expiration:</span> {item.expiration_date}</p>
+        <p><span>expiration:</span> {formatDate(item.expiration_date)}</p>
         <p><span>amount paid:</span> ${(item.amount_paid/100).toFixed(2)}</p>
     </div>
     <div>
