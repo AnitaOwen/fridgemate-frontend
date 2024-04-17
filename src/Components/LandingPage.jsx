704733cf9bf373e054f89b76214ed45a1e1b686d
AnitaOwen <anitaowen@pursuit.org>
2024-04-17 12:18:03 -0400

    added blurb to landing page

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -2,13 +2,14 @@ import { Link } from "react-router-dom";
 
 function LandingPage({ toggleLogin }) {
   return (
-    <div className="mt-5 text-center">
+    <div className="mt-4 text-center">
       <h1 className="mb-4">Welcome!</h1>
+      <h5>Manage fridge freshness, track expiration dates, and minimize food waste.</h5>
+      <h5 className="mb-4">Simplify your kitchen routine today!</h5>
       <img src="https://res.cloudinary.com/dveesfude/image/upload/v1712932028/giphy.webp_aujbo2.webp" alt="dancing fridge gif" />
-
       {toggleLogin ? (
         <>
-        <h4>You are already logged in!</h4>
+        <h4 className="mt-4">You are already logged in!</h4>
         <Link to="/fridges">Go to My Fridges</Link>
         </>
       ) : (
